fb0a4469428015df8b7c60369b39cdd4b4d91ef4
jp1593 <juanpablo08082002@hotmail.com>
2024-10-24 18:10:58 -0600

    explain result of not 3 example

--- Basic operations/Bitwise.cpp
+++ Basic operations/Bitwise.cpp
@@ -16,6 +16,17 @@ int main (){
     //NOT  takes two numbers, compares them bit by bit, and returns a new number where each bit is set (1) if the corresponding bits in the input numbers are different; otherwise, the bit is unset (0).
     int NOT = ~3; 
     std::cout << NOT << "\n"; 
+    //Explanation of why NOT 3 returns -4 as result:
+    // 1.  When you apply the NOT operator to 0000 0011, the result is: 1111 1100
+    // 2. Interpreting the Result:
+    // The result 1111 1100 is in two's complement format (the standard way of representing negative numbers in binary).
+    // To find the decimal value of this binary number, you can convert it back to decimal:
+    //     The leftmost bit is the sign bit (1 indicates a negative number).
+    //     To convert from two's complement, you invert the bits and add 1:
+    //         Invert 1111 1100 → 0000 0011
+    //         Add 1 → 0000 0100 (which is 4 in decimal)
+    //     Then ass the original binary was negative change the 4 to negative. 
+
 
     // Bitwise left shift operation (<<) is a binary operation that takes two numbers, a value and a shift amount, and returns a new number by shifting the bits of the value to the left by the specified shift amount. The vacated bits are filled with zeros. 
     int LEFTS = 5 << 3; 
@@ -25,4 +36,6 @@ int main (){
     int RIGHTS = 5 >> 2; 
     std::cout << RIGHTS<< "\n";
     return 0; 
+
+
 }
