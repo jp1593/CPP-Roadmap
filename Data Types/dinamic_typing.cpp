9d23136154dbd4ac76ef941da7678eb6841178b3
jp1593 <juanpablo08082002@hotmail.com>
2024-11-13 19:52:51 -0600

    feature that allow dinamic typing on c++

---
+++ Data Types/dinamic_typing.cpp
@@ -0,0 +1,38 @@
+/* A void* pointer is a generic pointer that can point to objects of any data type.They can be used to store a reference to
+any type of object without knowing the specific type of the objectinclude < iostream>
+*/
+
+#include <iostream>
+#include <any>
+
+int main()
+{
+    int x = 42;
+    float y = 3.14f;
+    std::string z = "Hello, world!";
+
+    void *void_ptr;
+
+    void_ptr = &x;
+    std::cout << "int value: " << *(static_cast<int *>(void_ptr)) << std::endl;
+
+    void_ptr = &y;
+    std::cout << "float value: " << *(static_cast<float *>(void_ptr)) << std::endl;
+
+    void_ptr = &z;
+    std::cout << "string value: " << *(static_cast<std::string *>(void_ptr)) << std::endl;
+
+    // C++17 introduced the std::any class which represents a generalized type-safe container for single values of any type.
+    std::any any_value;
+
+    any_value = 42;
+    std::cout << "int value: " << std::any_cast<int>(any_value) << std::endl;
+
+    any_value = 3.14;
+    std::cout << "double value: " << std::any_cast<double>(any_value) << std::endl;
+
+    any_value = std::string("Hello, world!");
+    std::cout << "string value: " << std::any_cast<std::string>(any_value) << std::endl;
+
+    return 0;
+}
