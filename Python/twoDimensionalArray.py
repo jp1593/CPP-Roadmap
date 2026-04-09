e3feab7ad4d4863c94f050259855970a830577a4
jp1593 <juanpablo08082002@hotmail.com>
2026-04-09 16:17:50 -0600

    feat: insert and append in 2d arrays

--- Python/twoDimensionalArray.py
+++ Python/twoDimensionalArray.py
@@ -7,3 +7,20 @@ import numpy as np
 
 week_data = np.array([[11,15,10,6],[10,24,11,5],[12,17,12,8],[15,18,14,9]])
 print(week_data)
+
+# Append a column value
+""" Insert and append methods """
+new_week = np.insert(week_data, len(week_data), [[3,6,9,2]], axis=1)
+print("\n ", new_week)
+
+new_week = np.append(week_data, [[3], [6],[9],[2]], axis=1)
+print("\n ", new_week)
+
+
+# Append a row value
+""" Insert and append methods """
+new_week = np.insert(week_data, 0, [[47,12,34,2]], axis=0)
+print("\n ", new_week)
+
+new_week = np.append(week_data, [[1,2,3,4]], axis=0)
+print("\n ", new_week)
