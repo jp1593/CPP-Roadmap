566b4f964851a23000eef70a475096d99a3a9489
jp1593 <juanpablo08082002@hotmail.com>
2026-04-16 12:56:45 -0600

    feat: add list topics and first examples

---
+++ Python/Lists/lists.py
@@ -0,0 +1,15 @@
+"""
+Examples of Lists
+
+# Integer List
+integers = [1,2,3,4]
+print(integers)
+
+# String list
+string_list = ['Cheese', 'Meeat', 'Bread']
+print(string_list)
+
+# Mixed List
+mixed_list = ['Hello', 'There', 0.5, [1,2,3], 'VIC']
+print(mixed_list)
+"""
