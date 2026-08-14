89801b9604944c0d33e360a8cdcf0c01382b7e27
jp1593 <juanpablo08082002@hotmail.com>
2026-08-14 14:21:25 -0600

    feat: prepend method to add node at the begin of the list

--- Python/LinkedList/appendMethod.py
+++ Python/LinkedList/appendMethod.py
@@ -9,26 +9,51 @@ class SinglyList:
         self.tail = None
         self.length = 0
 
+    def prepend(self, value): 
+        new_node = Node(value)
+        if self.head is None: 
+            self.head = new_node
+            self.tail = new_node
+
+        # Method to add node at the beggining fo the Linked List 
+        else: 
+            previous_head = self.head
+            self.head = new_node
+            self.head.next = previous_head
+        self.length += 1
+
     def append(self, value): 
         new_node = Node(value)
         if self.head is None: 
             self.head = new_node
             self.tail = new_node
-            
+
         # Method to add node at the end fo the Linked List 
         else: 
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
 
+    # String representation of an Instance
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None: 
+            result += str(temp_node.value)
+            if temp_node.next is not None: 
+               result += '-->'
+            # self.head.next = temp_node
+            temp_node = temp_node.next
+        return result
+
 linked_list = SinglyList() 
-print(linked_list.length)
 linked_list.append(107)
-print(linked_list.length)
 linked_list.append(70)
-print(linked_list.length)
-print(linked_list.head.value)
-print(linked_list.tail.value)
+linked_list.append(88)
+linked_list.append(63)
+linked_list.prepend(1)
+print(linked_list) 
+
 
 
 
