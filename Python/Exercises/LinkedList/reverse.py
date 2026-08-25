1f7de8626f9005e90b627cb65b8d56a015405cf6
jp1593 <juanpablo08082002@hotmail.com>
2026-08-25 12:59:55 -0600

    feat: reverse linked list exercise

---
+++ Python/Exercises/LinkedList/reverse.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += ' -> '
+            temp_node = temp_node.next
+        return result
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        
+    def reverse(self):
+        prev_node = None
+        current_node = self.head
+        for _ in range(self.length):
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node 
+            current_node = next_node
+        self.tail = self.head
+        self.head = prev_node
+
+
+linked_list = LinkedList() 
+linked_list.append(10)
+linked_list.append(20)
+linked_list.append(30)
+linked_list.append(40)
+linked_list.append(50)
+print(linked_list)
+linked_list.reverse()
+print(linked_list)
+print(linked_list.head.value, linked_list.tail.value)
