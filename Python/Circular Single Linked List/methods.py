0bedcb69d6030c14658b1091b79cc0d9f2530399
jp1593 <juanpablo08082002@hotmail.com>
2026-08-26 15:22:33 -0600

    feat: add append method of csll

---
+++ Python/Circular Single Linked List/methods.py
@@ -0,0 +1,28 @@
+class Node: 
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class CSLinkedList: 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value): 
+        new_node = Node(value)
+        if self.length == 0 : 
+            self.head = new_node 
+            self.tail = new_node
+            new_node.next = new_node 
+        else: 
+            self.tail.next = new_node 
+            self.tail = new_node 
+            new_node.next = self.head
+        self.length += 1
+
+cslinkedlist = CSLinkedList()
+cslinkedlist.append(1)
+cslinkedlist.append(2)
+cslinkedlist.append(3)
+print(cslinkedlist.head.value, cslinkedlist.tail.value)
