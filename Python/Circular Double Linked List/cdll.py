d9a516edf4e202634f993f4c00e98d15e8f62139
jp1593 <juanpablo08082002@hotmail.com>
2026-09-08 16:36:10 -0600

    feat: add append method to the cdll

--- Python/Circular Double Linked List/cdll.py
+++ Python/Circular Double Linked List/cdll.py
@@ -9,3 +9,18 @@ class CircularDoubleLinkedList:
         self.head = None 
         self.tail =  None 
         self.length = 0
+
+    def append(self, value): 
+        new_node = Node(value)
+        if not self.head: 
+            new_node.previous = new_node 
+            new_node.next = new_node 
+            self.head = new_node 
+            self.tail = new_node 
+        else: 
+            new_node.previous = self.tail 
+            new_node.next = self.head 
+            self.head.previous = new_node
+            self.tail.next = new_node 
+            self.tail = new_node 
+        self.length += 1
